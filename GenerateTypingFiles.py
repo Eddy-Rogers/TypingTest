3837b92b071b08fd2c40722d13d353d0339fdb1b
Eddy Rogers <eddyrogersiv@gmail.com>
2025-09-26 14:54:53 -0500

    Initial Development: Very simple capital word typing test.

---
+++ GenerateTypingFiles.py
@@ -0,0 +1,25 @@
+import random
+
+def main():
+    file = open("./Files/commonwords.csv", "r")
+
+    text = file.read()
+
+    wordarray = text.split(',')
+    cleanarray = []
+
+    for word in wordarray:
+        clean = word[0:word.find("(")-1].strip()
+
+        if len(clean) > 1 and clean.find(" ") == -1 and clean.find(".") == -1:
+            cleanarray.append(clean.capitalize())
+
+    random.shuffle(cleanarray)
+
+    wfile = open("./Outputs/CapitalTypingTest.txt", "x")
+
+    for word in cleanarray:
+        wfile.write(word + " ")
+
+if __name__ == "__main__":
+    main()
